8457d66230178b09bafe4e895dd666ad391cf149
inity8 <di848588@gmail.com>
2020-05-24 00:32:12 +0300

    show turtle

--- game.py
+++ game.py
@@ -3,7 +3,19 @@ import tkinter
 import turtle
 
 window = turtle.Screen()
-
+window.bgcolor('#85869E')
 pen = turtle.Turtle()
+pen.color('#25415B')
+for current_x in [-400, -200, 0, 200, 400]:
+    pen.setpos(x=current_x, y=0)
+    pen.circle(radius=100)
+    pen.forward(100)
+    pen.circle(radius=100)
+
+window.mainloop()
+
+
+
+
 
 window.mainloop()
