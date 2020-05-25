0cc118738060956ebf09791833185f3b768879ba
inity8 <di848588@gmail.com>
2020-05-25 23:12:26 +0300

    add images

--- game.py
+++ game.py
@@ -1,21 +1,32 @@
 # noinspection PyUnresolvedReferences
 import tkinter
 import turtle
+import random
 
 window = turtle.Screen()
-window.bgcolor('#85869E')
-pen = turtle.Turtle()
-pen.color('#25415B')
-for current_x in [-400, -200, 0, 200, 400]:
-    pen.setpos(x=current_x, y=0)
-    pen.circle(radius=100)
-    pen.forward(100)
-    pen.circle(radius=100)
+window.setup(600 + 3, 400 + 3)
+# window.bgcolor('#85869E')
+window.bgpic('images/background.png')
+window.screensize(1200, 800)
+
+def airplane(y):
+    pen = turtle.Turtle()
+    if y < 0:
+        pen.color('#ea483c')
+    else:
+        pen.color('#d0ea3e')
+    # pen.shape('triangle')
+    for current_x in [-100, 0, 100]:
+        pen.penup()
+        pen.setpos(x=current_x, y=y)
+        pen.pendown()
+        # pen.circle(radius=50)
+        pen.forward(100)
+        pen.circle(radius=random.randint(40, 80))
+
+airplane(y=100)
+airplane(y=-100)
 
 window.mainloop()
 
 
-
-
-
-window.mainloop()
