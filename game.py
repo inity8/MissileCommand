62608bb9574ab520ed405702e35367870057c30f
inity8 <di848588@gmail.com>
2020-05-25 23:47:43 +0300

    calculate heading

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # noinspection PyUnresolvedReferences
+import math
 import tkinter
 import turtle
 
@@ -9,22 +10,34 @@ window.screensize(1200, 800)
 
 BASE_X, BASE_Y = 0, -300
 
-missile = turtle.Turtle()
-missile.color('#d0ea3e')
-missile.penup()
-missile.setpos(x=BASE_X, y=BASE_Y)
-missile.pendown()
-missile.setheading(60)
-missile.forward(500)
-missile.shape('circle')
-missile.shapesize(2)
-missile.shapesize(3)
-missile.shapesize(4)
-# missile.shapesize(5)
-missile.clear()
-missile.hideturtle()
 
-window.onclick()
+def calc_heading(x1, y1, x2, y2):
+    dx = (x2-x1)
+    dy = (y2-y1)
+    length = (dx ** 2 + dy ** 2) ** 0.5
+    cos_alpha = dx / length
+    alpha = math.degrees(math.acos(cos_alpha))
+    return alpha
+
+
+def fire_missile(x, y):
+    missile = turtle.Turtle()
+    missile.color('#d0ea3e')
+    missile.penup()
+    missile.setpos(x=BASE_X, y=BASE_Y)
+    missile.pendown()
+    heading = calc_heading(x1=BASE_X, y1=BASE_Y, x2=x, y2=y)
+    missile.setheading(heading)
+    missile.forward(500)
+    missile.shape('circle')
+    missile.shapesize(2)
+    missile.shapesize(3)
+    missile.shapesize(4)
+    missile.clear()
+    missile.hideturtle()
+
+
+window.onclick(fire_missile)
 
 window.mainloop()
 
