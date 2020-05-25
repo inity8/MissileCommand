91fcc761506ceb2a3077ebd413eaf0cf223a5f51
inity8 <di848588@gmail.com>
2020-05-26 00:04:19 +0300

    refactoring

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ window = turtle.Screen()
 window.setup(1200 + 3, 800 + 3)
 window.bgpic('images/background.png')
 window.screensize(1200, 800)
+window.tracer(2)
 
 BASE_X, BASE_Y = 0, -300
 
@@ -23,25 +24,19 @@ def calc_heading(x1, y1, x2, y2):
 def fire_missile(x, y):
     # global missile
     missile = turtle.Turtle()
+    missile.hideturtle()
     missile.color('#d0ea3e')
     missile.penup()
     missile.setpos(x=BASE_X, y=BASE_Y)
     missile.pendown()
     heading = calc_heading(x1=BASE_X, y1=BASE_Y, x2=x, y2=y)
     missile.setheading(heading)
-    # missile.forward(500)
-    # missile.shape('circle')
-    # missile.shapesize(2)
-    # missile.shapesize(3)
-    # missile.shapesize(4)
-    # missile.clear()
-    # missile.hideturtle()
+    missile.showturtle()
     missiles.append(missile)
 
 
 window.onclick(fire_missile)
 missiles = []
-# missile = None
 
 while True:
     window.update()
