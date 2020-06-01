71f8266e693d2f3bdb4808b02c4e1ea00c64dd19
inity8 <di848588@gmail.com>
2020-06-01 03:43:45 +0300

    add info missile states

--- game.py
+++ game.py
@@ -7,19 +7,20 @@ window = turtle.Screen()
 window.setup(1200 + 3, 800 + 3)
 window.bgpic('images/background.png')
 window.screensize(1200, 800)
-# window.tracer(2)
+window.tracer(2)
 
 BASE_X, BASE_Y = 0, -300
 
 
 def calc_heading(x1, y1, x2, y2):
     dx = (x2-x1)
-    dy = y2 - y1
+    dy = (y2-y1)
     length = (dx ** 2 + dy ** 2) ** 0.5
     cos_alpha = dx / length
-    alpha = math.degrees(math.acos(cos_alpha))
+    alpha = math.acos(cos_alpha)
+    alpha = math.degrees(alpha)
     if dy < 0:
-        alpha = -alpha
+        alpha = - alpha
     return alpha
 
 
@@ -34,35 +35,41 @@ def fire_missile(x, y):
     heading = calc_heading(x1=BASE_X, y1=BASE_Y, x2=x, y2=y)
     missile.setheading(heading)
     missile.showturtle()
-    our_missiles.append(missile)
-    our_missiles_target.append([x, y])
-    our_missiles_states.append('launched')
-    our_missiles_radius.append(0)
+
+    info = {'missile': missile,
+            'target': [x, y],
+            'state': 'launched',
+            'radius': 0
+            }
+    our_missiles.append(info)
 
 
 window.onclick(fire_missile)
 our_missiles = []
-our_missiles_target = []
-our_missiles_states = []
-our_missiles_radius = []
 
 
 while True:
     window.update()
 
-    for num, missile in enumerate(our_missiles):
-        if our_missiles_states[num] == 'launched':
+    for info in our_missiles:
+        state = info['state']
+        missile = info['missile']
+
+        if state == 'launched':
             missile.forward(4)
-            target = our_missiles_target[num]
+            target = info['target']
             if missile.distance(x=target[0], y=target[1] < 20):
-                our_missiles_states[num] = 'explode'
+                info['state'] = 'explode'
                 missile.shape('circle')
-        elif our_missiles_states[num] == 'explode':
-            our_missiles_radius[num] += 1
-            missile.shapesize(our_missiles_radius[num])
-            # missile.shapesize(2)
-            # missile.shapesize(3)
-            # missile.shapesize(4)
-            # missile.shapesize(5)
+        elif state == 'explode':
+            info['radius'] += 1
+            if info['radius'] > 5:
+                info['state'] = 'dead'
+            else:
+                missile.shapesize(info['radius'])
+        elif state == 'dead':
+            missile.clear()
+            missile.hiderurtle()
+
 
 
